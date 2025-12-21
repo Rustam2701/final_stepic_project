f50a631ec9ae514d32ea2f5c60dfabfe44b78612
Kodzokov Rustam <kodzokov@kavichki.com>
2025-12-21 22:53:19 +0300

    First commit

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en-gb",
+        help="Choose language: ru or en-gb"
+    )
+
+
+@pytest.fixture(scope="session")
+def language(request):
+    return request.config.getoption("--language")
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
